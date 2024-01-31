c95038bb48ac5da62163cb6d7117d56bfbbc4ce6
John An <junghooonan@gmail.com>
2024-01-30 17:13:42 -0800

    fixed NavLink routers

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,42 +1,39 @@
 import './App.css';
 import {
-  Home
+  Home,
+  CardForm,
+  CardsList,
+  Navbar
 } from "./Components/index";
 import {
   BrowserRouter,
   Routes,
   Route
 } from 'react-router-dom';
+import styles from './style';
 
 
 function App() {
   const domain = /https:\/\/[^/]+/;
   const basename = process.env.PUBLIC_URL.replace(domain, "");
   return (
-    <div>
-      <BrowserRouter basename={basename}>
-        <Routes>
-          <Route exact path="/" element={<Home />} >
-            <Route path="cards/">
-              {/* <Route path="create" element={<CardForm />} />
-              <Route index element={<CardsList />} />
-              <Route path=":community_id" element={<CardDetail />} /> */}
-            </Route>
-            <Route path="budgets/">
-              {/* <Route path="all" element={<AllEventList />} />
-              <Route path="create" element={<EventForm />} />
-              <Route path=":event_id" element={<EventDetails />} /> */}
-            </Route>
-            <Route path="transactions/">
-              {/* <Route path="all" element={<AllEventList />} />
-              <Route path="create" element={<EventForm />} />
-              <Route path=":event_id" element={<EventDetails />} /> */}
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="cards/">
+          <Route index element={<CardsList />} />
+        </Route>
+        <Route path="budgets/">
+          <Route></Route>
+          <Route></Route>
+        </Route>
+        <Route path="transactions/">
+          <Route></Route>
+          <Route></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+);
 }
 
 export default App;
